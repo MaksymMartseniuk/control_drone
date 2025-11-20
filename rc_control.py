78570e5908be1cac3b4eef7cf790194e07e3bfac
MaxMartseniuk <marcenukm995@gmail.com>
2025-11-20 21:43:31 +0200

    Enhance RC and Vehicle Control Nodes with landing functionality and improved service integration

--- rc_control.py
+++ rc_control.py
@@ -1,12 +1,11 @@
 import rclpy
 from rclpy.node import Node
 from ros_px4_my.msg import RCControl
-from ros_px4_my.srv import TakeOff
+from ros_px4_my.srv import TakeOff, Land
 import sys
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
 import threading
 import time
-from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
 if sys.platform == 'win32':
     import msvcrt
@@ -25,6 +24,8 @@ R: Скидання всіх команд швидкості до 0.0
 
 Space: Arm / Disarm
 Q: Перехід в режим Offboard (Після ARM)
+T: Takeoff (Зліт на 5м)
+L: Land (Автоматична посадка)
 
 CTRL-C: Вихід
 ===================================
@@ -60,7 +61,6 @@ def restore_terminal_settings(old_settings):
 class RCControlNode(Node):
     def __init__(self):
         super().__init__('rc_control_node')
-
         self.get_logger().info(msg)
 
         qos_reliable = QoSProfile(
@@ -69,23 +69,22 @@ class RCControlNode(Node):
             depth=10,
             durability=QoSDurabilityPolicy.TRANSIENT_LOCAL
         )
-        
+        # ---PUBLISHERS---
         self.rc_command_publisher = self.create_publisher(
             RCControl,
             'rc_command',
             qos_reliable
         )
+        # ---CLIENT---
+        self.takeoff_client = self.create_client(TakeOff, 'takeoff_command/drone')
+        self.land_client = self.create_client(Land, 'land_command/drone')
 
-        self.takeoff_client=self.create_client(
-            TakeOff,
-            'takeoff_command/drone'
-        )
-
+        # ---LOCAL STATE---
         self.arm_state = False
         self.offboard_state = False
         
-        self.throttle_step = 0.1
-        self.rate_step = 0.15
+        self.throttle_step = 0.05
+        self.rate_step = 0.1
         self.max_rate = 1.5
         self.max_yaw_rate = 2.0
         self.max_throttle = 1.0 
@@ -133,21 +132,22 @@ class RCControlNode(Node):
             '\x1b[C': lambda: setattr(self, 'target_roll_rate', min(self.max_rate, self.target_roll_rate + self.rate_step)),  # arrow right
             '\x1b[D': lambda: setattr(self, 'target_roll_rate', max(-self.max_rate, self.target_roll_rate - self.rate_step)),  # arrow left
             'r': self.reset_controls,
-            't':self.takeoff_drone_command
+            't': self.takeoff_drone_command,
+            'l': self.land_drone_command,
         }
         
         try:
             while rclpy.ok():
                 key = get_key()
                 
-                if key == '\x03':
+                if key == '\x03': # CTRL-C
                     self.get_logger().info("CTRL-C detected, shutting down...")
                     rclpy.shutdown()
                     break
 
                 if key == ' ':
                     self.arm_state = not self.arm_state
-                    self.get_logger().warn(f'ARM STATE: {"ARMED" if self.arm_state else "DISARMED"}')
+                    self.get_logger().warn(f'ARM STATE REQUEST: {"ARMED" if self.arm_state else "DISARMED"}')
                     if not self.arm_state:
                         self.offboard_state = False
                         self.reset_controls()
@@ -156,7 +156,7 @@ class RCControlNode(Node):
                 elif key.lower() == 'q':
                     if self.arm_state:
                         self.offboard_state = True
-                        self.get_logger().warn('OFFBOARD MODE ENABLED')
+                        self.get_logger().warn('OFFBOARD MODE REQUESTED')
                     else:
                         self.get_logger().error('Cannot enter Offboard mode while disarmed!')
                     continue
@@ -164,11 +164,9 @@ class RCControlNode(Node):
                 if self.arm_state and self.offboard_state:
                     if key in key_actions:
                         key_actions[key]()
-                else:
-                    self.target_roll_rate = 0.0
-                    self.target_pitch_rate = 0.0
-                    self.target_yaw_rate = 0.0
-                    self.throttle_target = 0.0
+
+                elif key in ['t', 'l']:
+                     key_actions[key]()
 
                 self.target_roll_rate = max(-self.max_rate, min(self.max_rate, self.target_roll_rate))
                 self.target_pitch_rate = max(-self.max_rate, min(self.max_rate, self.target_pitch_rate))
@@ -190,34 +188,61 @@ class RCControlNode(Node):
             f"Rol: {self.target_roll_rate: .1f} | "
             f"Yaw: {self.target_yaw_rate: .1f}"
         )
-        self.get_logger().info(status_msg)
+
+        sys.stdout.write(f"\r{status_msg}")
+        sys.stdout.flush()
 
     def takeoff_drone_command(self):
         if not self.arm_state:
-            self.get_logger().error("Cannot takeoff while disarmed!")
+            self.get_logger().error("\nCannot takeoff while disarmed! Press SPACE first.")
             return
-        if not self.takeoff_client.wait_for_service(timeout_sec=2.0):
-            self.get_logger().error("TakeOff service not available!")
+        if not self.takeoff_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().error("\nTakeOff service not available!")
             return
         
         req = TakeOff.Request()
-        req.target_height=5.0
+        req.target_height = 5.0
         future = self.takeoff_client.call_async(req)
-        self.get_logger().info("Sending takeoff command...")
+        self.get_logger().info("\nSending takeoff command...")
+        
         def response_callback(fut):
             try:
                 resp = fut.result()
-                self.get_logger().info(f"Takeoff success: {resp.success}, message: {resp.message}")
+                self.get_logger().info(f"Takeoff response: {resp.message}")
+                if resp.success:
+                    self.offboard_state = True
             except Exception as e:
-                self.get_logger().error(f"Takeoff service call failed: {e}")
+                self.get_logger().error(f"Takeoff call failed: {e}")
 
+        future.add_done_callback(response_callback)
+
+    def land_drone_command(self):
+        if not self.land_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().error("\nLanding service not available")
+            return
+
+        req = Land.Request()
+        self.get_logger().warn("\nSending LAND command...")
+        
+        future = self.land_client.call_async(req)
+        
+        def response_callback(fut):
+            try:
+                resp = fut.result()
+                self.get_logger().info(f"Landing response: {resp.message}")
+                if resp.success:
+                    self.offboard_state = False
+                    self.arm_state = False 
+                    self.reset_controls()
+            except Exception as e:
+                self.get_logger().error(f"Landing call failed: {e}")
+                
         future.add_done_callback(response_callback)
 
 def main(args=None):
     old_settings = save_terminal_settings()
-
     rclpy.init(args=args)
-    node=None
+    node = None
     try:
         node = RCControlNode()
         rclpy.spin(node)
@@ -230,7 +255,6 @@ def main(args=None):
             node.destroy_node()
         if rclpy.ok():
             rclpy.shutdown()
-        
         restore_terminal_settings(old_settings)
 
 if __name__ == '__main__':
--- vehicle_control.py
+++ vehicle_control.py
@@ -4,9 +4,8 @@ from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDur
 from rclpy.executors import MultiThreadedExecutor
 from px4_msgs.msg import OffboardControlMode, VehicleCommand, VehicleRatesSetpoint, TrajectorySetpoint
 from ros_px4_my.msg import RCControl 
-from ros_px4_my.srv import TakeOff
+from ros_px4_my.srv import TakeOff, Land
 from sensor_msgs.msg import LaserScan
-from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
 class VehicleControlNode(Node):
     def __init__(self):
@@ -26,9 +25,10 @@ class VehicleControlNode(Node):
             durability=QoSDurabilityPolicy.VOLATILE
         )
         
-        # --- Publishers ---
+        # ---PUBLISHERS---
         self.cmd_publisher_ = self.create_publisher(
             VehicleCommand, '/fmu/in/vehicle_command', qos_reliable)
+        
         self.offboard_mode_publisher_ = self.create_publisher(
             OffboardControlMode, '/fmu/in/offboard_control_mode', qos_reliable)
         
@@ -37,27 +37,20 @@ class VehicleControlNode(Node):
         
         self.position_setpoint_publisher_ = self.create_publisher(
             TrajectorySetpoint, "/fmu/in/trajectory_setpoint", qos_best_effort)
-        
-        # --- Subscribers ---
+        # ---SUBSCROBERS---
         self.rc_command_subscriber = self.create_subscription(
-            RCControl,
-            'rc_command',
-            self.rc_command_callback,
-            qos_reliable
-        )
+            RCControl, 'rc_command', self.rc_command_callback, qos_reliable)
 
-        self.lidar_subscriber=self.create_subscription(
+        self.lidar_subscriber = self.create_subscription(
             LaserScan,
             '/world/default/model/x_lidar_my_0/link/lidar_sensor_link/sensor/lidar/scan',
             self.lidar_callback,
             qos_best_effort
         )
-        # ---Server---
-        self.takeoff_service=self.create_service(
-            TakeOff,
-            'takeoff_command/drone',
-            self.takeoff_callback,)
-        # ---State---
+        # ---SERVICE---
+        self.takeoff_service = self.create_service(TakeOff, 'takeoff_command/drone', self.takeoff_callback)
+        self.land_service = self.create_service(Land, 'land_command/drone', self.land_callback)
+        # ---LOCALSTATE---
         self.arm_state = False
         self.offboard_state = False
         self.armed_state_sent = False 
@@ -66,27 +59,46 @@ class VehicleControlNode(Node):
         self.offboard_setpoint_counter = 0
         self.offboard_mode_sent = False
 
+        self.min_lidar_distance = float('inf')
+
         self.current_rc_control = RCControl()
 
-        # ---Timer---    
-        self.timer = self.create_timer(0.01, self.timer_callback)
+        self.timer = self.create_timer(0.02, self.timer_callback)
+        
         self.get_logger().info("VehicleControlNode initialized.")
-        self.get_logger().info('Lidar subscriber started.')
+
+    
+    def land_callback(self, request, response):
+        self.get_logger().info("Landing Service Called.")
+        
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND)
+        
+        self.offboard_state = False 
+        self.current_setpoint_height = None
+        self.offboard_mode_sent = False
+        self.offboard_setpoint_counter = 0
+        
+        self.arm_state = True 
+        
+        response.success = True
+        response.message = "PX4 Land Mode triggered."
+        self.get_logger().info(response.message)
+        return response
 
     def takeoff_callback(self, request, response):
         if not self.arm_state:
             response.success = False
-            response.message = "Cannot takeoff: Drone is disarmed."
+            response.message = "Rejected: Drone disarmed."
             self.get_logger().error(response.message)
             return response
 
         self.current_setpoint_height = request.target_height
         self.offboard_state = True
+        
         response.success = True
-        response.message = f"Takeoff command sent to {self.current_setpoint_height} meters."
+        response.message = f"Takeoff initiated to {self.current_setpoint_height}m."
         self.get_logger().info(response.message)
         return response
-
 
     def rc_command_callback(self, msg: RCControl):
         self.current_rc_control = msg
@@ -100,21 +112,14 @@ class VehicleControlNode(Node):
     def lidar_callback(self, msg: LaserScan):
         if msg.ranges:
             valid_ranges = [r for r in msg.ranges if r != float('inf') and r > msg.range_min]
-            
-            if valid_ranges:
-                min_range = min(valid_ranges)
-            else:
-                min_range = float('inf')
+            self.min_lidar_distance = min(valid_ranges) if valid_ranges else float('inf')
         else:
-            min_range = float('inf')
-            
-        self.get_logger().info(
-            f"Recieve data from LaserScan: "
-            f"Min distance {min_range:.2f}m. "
-        )
+            self.min_lidar_distance = float('inf')
 
-        if min_range >0.2 and min_range < 0.5:
-            self.get_logger().warn("COLLISION WARNING: Object detected closer than 0.5m!")
+        if self.arm_state:
+            if 0.1 < self.min_lidar_distance < 0.5:
+                self.get_logger().warn(f"COLLISION WARNING: Object at {self.min_lidar_distance:.2f}m!")
+   
    
     def timer_callback(self):
         if not rclpy.ok():
@@ -122,26 +127,26 @@ class VehicleControlNode(Node):
 
         timestamp = int(self.get_clock().now().nanoseconds / 1000)
 
-        # ARM/DISARM
         if self.arm_state and not self.armed_state_sent:
-            self.public_arm_disarm(True)
+            self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=1.0)
             self.armed_state_sent = True
         elif not self.arm_state and self.armed_state_sent:
-            self.public_arm_disarm(False)
+            self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=0.0)
             self.armed_state_sent = False
+           
             self.offboard_state = False
             self.offboard_setpoint_counter = 0
             self.offboard_mode_sent = False
 
-        # --- OFFBOARD LOGIC ---
         if self.offboard_state:
+            
             rc_nonzero = (
                 abs(self.current_rc_control.target_roll_rate) > 0.0 or
                 abs(self.current_rc_control.target_pitch_rate) > 0.0 or
                 abs(self.current_rc_control.target_yaw_rate) > 0.0 or
                 abs(self.current_rc_control.throttle_target) > 0.0
                 )
-            # Messaging mode
+            
             offboard_msg = OffboardControlMode()
             offboard_msg.timestamp = timestamp
             offboard_msg.position = False
@@ -150,41 +155,40 @@ class VehicleControlNode(Node):
             offboard_msg.attitude = False
             offboard_msg.body_rate = False
 
-            # --- POSITION TAKEOFF MODE ---
             if self.current_setpoint_height is not None and not rc_nonzero:
                 offboard_msg.position = True
             
                 traj_msg = TrajectorySetpoint()
                 traj_msg.timestamp = timestamp
-                traj_msg.position = [0.0, 0.0, -self.current_setpoint_height]
+                traj_msg.position = [0.0, 0.0, -float(self.current_setpoint_height)]
                 traj_msg.yaw = 0.0
 
                 self.position_setpoint_publisher_.publish(traj_msg)
 
-            # --- BODY RATE MANUAL CONTROL ---
             else:
                 self.current_setpoint_height = None
                 offboard_msg.body_rate = True
 
                 rates_msg = VehicleRatesSetpoint()
                 rates_msg.timestamp = timestamp
-                rates_msg.roll = self.current_rc_control.target_roll_rate
-                rates_msg.pitch = self.current_rc_control.target_pitch_rate
-                rates_msg.yaw = self.current_rc_control.target_yaw_rate
-                rates_msg.thrust_body = [0.0, 0.0, -self.current_rc_control.throttle_target]
+                rates_msg.roll = float(self.current_rc_control.target_roll_rate)
+                rates_msg.pitch = float(self.current_rc_control.target_pitch_rate)
+                rates_msg.yaw = float(self.current_rc_control.target_yaw_rate)
+                rates_msg.thrust_body = [0.0, 0.0, -float(self.current_rc_control.throttle_target)]
 
                 self.rates_setpoint_publisher_.publish(rates_msg)
 
-            # Publish OffboardControlMode every cycle
             self.offboard_mode_publisher_.publish(offboard_msg)
 
-            # Count consecutive setpoints
             if self.offboard_setpoint_counter < 50:
                 self.offboard_setpoint_counter += 1
 
-            # Send SET_MODE only after 50 valid setpoints
             if self.offboard_setpoint_counter >= 50 and not self.offboard_mode_sent:
-                self.public_set_offboard_mode()
+                self.publish_vehicle_command(
+                    VehicleCommand.VEHICLE_CMD_DO_SET_MODE, 
+                    param1=1.0,
+                    param2=6.0
+                )
                 self.offboard_mode_sent = True
 
         else:
@@ -192,13 +196,11 @@ class VehicleControlNode(Node):
             self.offboard_mode_sent = False
 
 
-
-
-    def public_arm_disarm(self, arm: bool):
+    def publish_vehicle_command(self, command, param1=0.0, param2=0.0):
         msg = VehicleCommand()
-        msg.command = VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM
-        msg.param1 = 1.0 if arm else 0.0
-        msg.param2 = 2.0
+        msg.command = command
+        msg.param1 = param1
+        msg.param2 = param2
         msg.target_system = 1
         msg.target_component = 1
         msg.source_system = 255
@@ -206,24 +208,6 @@ class VehicleControlNode(Node):
         msg.from_external = True
         msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
         self.cmd_publisher_.publish(msg)
-        
-        status_str = "ARMED" if arm else "DISARMED"
-        self.get_logger().info(f"--- Drone {status_str} ---")
-
-    def public_set_offboard_mode(self):
-        msg = VehicleCommand()
-        msg.command = VehicleCommand.VEHICLE_CMD_DO_SET_MODE
-        msg.param1 = 1.0  
-        msg.param2 = 6.0
-        msg.target_system = 1
-        msg.target_component = 1
-        msg.source_system = 255
-        msg.source_component = 1
-        msg.from_external = True
-        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
-        self.cmd_publisher_.publish(msg)
-    
-        
 
 def main(args=None):
     rclpy.init(args=args)
